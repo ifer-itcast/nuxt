5f45e63b14bf58990084bf4a7ab951953107e326
ifer-itcast <yangkangkang@itcast.cn>
2021-03-18 23:47:12 +0800

    设置head中的meta信息

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    bodyAttrs: {
+    }
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        ssr
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      x
     </div>
   </div>
 </template>
@@ -33,46 +12,26 @@ export default {
   // layout() {
   //   return 'blog/index'
   // }
+  data() {
+    return {
+      title: '这是首页标题'
+    }
+  },
+  head() {
+    return {
+      title: this.title,
+      bodyAttrs: {
+        style: 'background-color: #fff'
+      },
+      meta: [
+        {
+          // 指定 hid 会覆盖 nuxt.config.js 中同名的 meta 配置
+          hid: 'description',
+          name: 'description',
+          content: 'nuxt.js 技术栈'
+        }
+      ]
+    }
+  }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
