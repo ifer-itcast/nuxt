0c53cd5d94e1fde12a1347fe4f6f70d85981fad5
ifer-itcast <yangkangkang@itcast.cn>
2021-03-19 23:31:31 +0800

    利用@nuxtjs/axios和asyncData加载数据

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
-    <div>
-      x
-    </div>
+    <h2>首页标题：{{title}}</h2>
+    <ul>
+      <li>标题：{{data.title}}</li>
+      <li>内容：{{data.content}}</li>
+      <li>作者：{{data.author}}</li>
+    </ul>
   </div>
 </template>
 
@@ -32,6 +35,17 @@ export default {
         }
       ]
     }
-  }
+  },
+  // 加载组件之前服务端会调用
+  /* asyncData({$axios}) {
+    return $axios.get('https://mock.mengxuegu.com/mock/6054bf950d58b864da03d161/blog-web/test').then(response => {
+      const data = response.data.data;
+      return { data };
+    });
+  }, */
+  async asyncData({$axios}) {
+    const { data: { data } } = await $axios.get('https://mock.mengxuegu.com/mock/6054bf950d58b864da03d161/blog-web/test')
+    return { data };
+  },
 }
 </script>
