e4d5178e768ef338ae77b25354004ebf2e5d2d48
ifer-itcast <yangkangkang@itcast.cn>
2021-03-20 22:40:38 +0800

    根据页面组件自动生成路由表

---
+++ pages/_question/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>问答列表{{$route.params.question}}</div>
+</template>
---
+++ pages/user/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>详情{{$route.params.id}}</div>
+</template>
+<script>
+export default {
+  // 校验请求参数，返回 true 则可以访问此组件
+  validate({params}) {
+    return /^\d+$/.test(params.id);
+  }
+}
+</script>
